6b7bbce0e6a064f302abb95bd3febeaa7b39c3a4
Shane Hwang <rollingfeet@gmail.com>
2020-06-13 10:38:14 +0900

    nestjs typeorm sqlite

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule { }
